803eaeaa7a9d04caee9215efb19c86bc850f3932
g11425 <gopi.g102525@gmail.com>
2025-12-07 09:36:10 +0530

    ETL updated : * filters out 0 fare, locations ids * added isWeekend column using pickup timestamp

--- pyspark/etl_spark.py
+++ pyspark/etl_spark.py
@@ -2,10 +2,18 @@ from pyspark.sql import SparkSession, functions as f
 
 spark = SparkSession.builder.appName("NYC_taxi_ETL").getOrCreate()
 
-data_path = "G:/DE/Projects/NYC_taxi/data/"
+src_data_path = "G:/DE/Projects/NYC_taxi/data/raw/"
+des_data_path = "G:/DE/Projects/NYC_taxi/data/processed/"
 file_name = "yellow_tripdata_2025-01.parquet"
 
-df = spark.read.parquet(data_path + file_name)
+df = spark.read.parquet(src_data_path + file_name)
 df.printSchema()
+
+df = df.filter((f.col("fare_amount") != 0) & (f.col("PULocationID") != 0) & (f.col("DOLocationID") != 0))
+df = df.withColumn("IsWeekend", f.when(f.dayofweek(f.col("tpep_pickup_datetime")).isin(1,7), 1).otherwise(0))
+
+print(df.count())
+
+df.write.mode("overwrite").parquet(des_data_path+file_name)
 
 spark.stop()
