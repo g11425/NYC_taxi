848d5f3bf7979d1839871f262f1e74f0478fff0c
g11425 <gopi.g102525@gmail.com>
2025-12-08 17:43:12 +0530

    setting sdk DAG

--- airflow/etl_flow_test.py
+++ airflow/etl_flow_test.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 from airflow.providers.standard.operators.bash import BashOperator
 
 # The DAG object; we'll need this to instantiate a DAG
-from airflow import DAG
+from airflow.sdk import DAG
 with DAG(
     "tutorial",
     # These args will get passed on to each operator
