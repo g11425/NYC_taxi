82f2b403b8b11a901c593e9bd5f5fddfad309f82
g11425 <gopi.g102525@gmail.com>
2025-12-08 15:54:40 +0530

    fixed DAG and bashOperator import

--- airflow/etl_flow_test.py
+++ airflow/etl_flow_test.py
@@ -2,10 +2,9 @@ import textwrap
 from datetime import datetime, timedelta
 
 # Operators; we need this to operate!
-from airflow.providers.standard.operators.bash import BashOperator
+from airflow import DAG
+from airflow.operators.bash import BashOperator
 
-# The DAG object; we'll need this to instantiate a DAG
-from airflow.sdk import DAG
 with DAG(
     "tutorial",
     # These args will get passed on to each operator
