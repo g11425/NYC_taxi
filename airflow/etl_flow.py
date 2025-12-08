e99fb0f00552fcdd6efc5f10ce7bf1f04bbf9fe2
g11425 <gopi.g102525@gmail.com>
2025-12-08 18:10:57 +0530

    fxing imports

--- airflow/etl_flow.py
+++ airflow/etl_flow.py
@@ -1,6 +1,9 @@
-from airflow import DAG
 from datetime import datetime, timedelta
-from airflow.operators.bash import BashOperator
+# Operators; we need this to operate!
+from airflow.providers.standard.operators.bash import BashOperator
+
+# The DAG object; we'll need this to instantiate a DAG
+from airflow.sdk import DAG
 
 with DAG(
     "NYC_taxi_flow",
