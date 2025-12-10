d7cde200da85cd6a28e3c9c1269763b195e7707c
g11425 <gopi.g102525@gmail.com>
2025-12-10 20:34:23 +0530

    adding test

---
+++ test/mkdir.py
@@ -0,0 +1,16 @@
+import os
+from urllib.request import urlretrieve
+
+
+file_path_raw = "~/NYC_taxi/data/raw/"
+file_path_processed = "~/NYC_taxi/data/processed/"
+file_name = "yellow_tripdata_2025-01.parquet"
+url = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2025-01.parquet'
+
+if not os.path.exists(file_path_raw):
+    os.mkdir(file_path_raw)
+if not os.path.exists(file_path_processed):
+    os.mkdir(file_path_processed)
+    
+urlretrieve(url, file_path_raw+file_name)
+print("successfully saved data")
