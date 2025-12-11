585f31e0483f5e875dddd4658617ac112693a4f5
g11425 <gopi.g102525@gmail.com>
2025-12-11 22:02:17 +0530

    aaa

--- test/test_fetch_data.py
+++ test/test_fetch_data.py
@@ -1,5 +1,5 @@
-# import airflow.etl_flow as flw 
+import airflow.etl_flow as flw 
 
-# def test_fetch_data():
-#     assert flw.fetch_data() == True
+def test_fetch_data():
+    assert flw.fetch_data() == True
 
