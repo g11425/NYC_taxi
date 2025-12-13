fa7de8d4008f26b17b97f5830708263443c64a86
g11425 <gopi.g102525@gmail.com>
2025-12-13 19:03:52 +0530

    fix test file path

--- airflow/FileOps.py
+++ airflow/FileOps.py
@@ -2,6 +2,7 @@ import logging
 import os
 import os.path
 import shutil
+from tkinter import SEL
 
 
 class FileOps:
@@ -12,7 +13,7 @@ class FileOps:
 		self.data_path = os.path.join(project_path, "data")
 		self.data_path_raw = os.path.join(self.data_path, "raw")
 		self.data_path_processed = os.path.join(self.data_path, "processed")
-		self.test_file_raw = os.path.join(self.data_path_raw, "test.csv")
+		self.test_file_raw = "test.csv"
 
 	def setup_data_paths(self):
 		
@@ -48,7 +49,7 @@ class FileOps:
 	def create_dummy_raw(self):
 		self.setup_data_paths()
 		try:
-			file=open(self.test_file_raw, mode="w")
+			file=open(self.get_test_raw_file_name_abs(), mode="w")
 			file.write("col1,col2,col3")
 			file.close()
 			return True
@@ -69,3 +70,6 @@ class FileOps:
 
 	def check_raw_path(self):
 		return os.path.exists(self.data_path_raw)
+
+	def get_test_raw_file_name_abs(self):
+		return os.path.join(self.data_path_raw, self.test_file_raw)
