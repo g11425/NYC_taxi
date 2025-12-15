0f57f92773e59af488dedc1f02d959dcfa77e3f9
g11425 <gopi.g102525@gmail.com>
2025-12-15 22:32:37 +0530

    fixing parameters

--- airflow/etl_flow.py
+++ airflow/etl_flow.py
@@ -212,11 +212,11 @@ with DAG(
         task_id="copy_to_redshfit",
         redshift_conn_id="redshfit_default",
         aws_conn_id="aws_default",
-        table="dev.test_table",
+        table="test_table",
         s3_bucket="s3-giam-bucket-001",
         s3_key="NYC_taxi/raw/2025/01/",
         method="APPEND",
-        schema="public"
+        schema="dev"
         )
 
     t4 >> t2 >> t5 >> t7
