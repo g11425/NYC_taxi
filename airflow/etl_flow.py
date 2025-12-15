216d1fd80ffdca06e828408ee3fd14a868c9ef74
g11425 <gopi.g102525@gmail.com>
2025-12-15 20:57:18 +0530

    fxing path

--- airflow/etl_flow.py
+++ airflow/etl_flow.py
@@ -214,8 +214,9 @@ with DAG(
         aws_conn_id="aws_default",
         table="dev.test_table",
         s3_bucket="s3-giam-bucket-001",
-        s3_key="NYC_taxi/processed/2025/01/",
-        method="APPEND"
+        s3_key="NYC_taxi/raw/2025/01/",
+        method="APPEND",
+        schema="public"
         )
 
     t4 >> t2 >> t5 >> t7
