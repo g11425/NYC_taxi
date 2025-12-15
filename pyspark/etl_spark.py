5bf7211e776200070c7d7604011d769588923095
g11425 <gopi.g102525@gmail.com>
2025-12-15 22:52:32 +0530

    test

--- pyspark/etl_spark.py
+++ pyspark/etl_spark.py
@@ -9,7 +9,7 @@ file_name = "yellow_tripdata_2025-01.parquet"
 
 df = spark.read.parquet(src_data_path + file_name)
 df.printSchema()
-
+df.show(10)
 df = df.filter((f.col("fare_amount") != 0) & (f.col("PULocationID") != 0) & (f.col("DOLocationID") != 0))
 df = df.withColumn("IsWeekend", f.when(f.dayofweek(f.col("tpep_pickup_datetime")).isin(1,7), 1).otherwise(0))
 
