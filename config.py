7c6864a7dfd248cf14bc71cbaa81e5b8656fb9b5
g11425 <gopi.g102525@gmail.com>
2026-01-08 18:27:14 +0530

    added dimension lookups and stats

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import computed_field
 
 
 
@@ -18,9 +19,66 @@ S3_RAW_KEY = "NYC_taxi/raw/2025/01/"
 S3_PRCSD_KEY = "NYC_taxi/processed/2025/01/"
 FILE_NAME = "yellow_tripdata_2025-01.parquet"
 
+
+
 class Settings(BaseSettings):
-    S3_RAW_TEST_FILE_KEY:str =S3_TEST_RAW_KEY+S3_RAW_TEST_FILE
     PROJECT_ROOT:str = os.path.dirname(os.path.abspath(__file__))
+    EXEC_ENV:str = "emr" # emr or local
+
+    S3_TEST_BUCKET:str = "s3-giam-bucket-001"
+    S3_TEST_RAW_KEY:str = "NYC_taxi/raw/2025/01/"
+    S3_RAW_TEST_FILE:str ="yellow_tripdata_2025-01.parquet"
+    TEST_RAW_FILE_URL:str ='https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2025-01.parquet'
+    S3_BUCKET:str = "s3-giam-bucket-001"
+    LOCAL_RAW_DATA_PATH:str = "/home/ec2-user/NYC_taxi/data/raw/"
+    LOCAL_PRCSD_DATA_PATH:str = "/home/ec2-user/NYC_taxi/data/processed/"
+
+    S3_RAW_KEY:str = "NYC_taxi/raw/2025/01/"
+    S3_PRCSD_KEY:str = "NYC_taxi/processed/2025/01/"
+    FILE_NAME:str = "yellow_tripdata_2025-01.parquet"
+    ETL_PY_PATH_REL:str = "airflow"
+    ETL_PY_FILE_NAME:str = "etl_spark_emr.py"
+    S3_ETL_PY_PREFIX:str = "NYC_taxi"
+    PAYMENT_TYPE_FILE:str = "paymenttype_lookup.csv"
+    RATE_CODE_FILE:str = "ratecode_lookup.csv"
+    TAXI_ZONE_FILE:str = "taxi_zone_lookup.csv"
+    VENDOR_LOOKUP_FILE:str = "vendor_lookup.csv"
+    S3_EXTRAS:str = "/NYC_taxi/processed/2025/01/extras"
+    LOCAL_EXTRAS:str = "extras"
+
+
+
+    @computed_field
+    @property
+    def S3_RAW_TEST_FILE_KEY(self):
+        return os.path.join( self.S3_TEST_RAW_KEY, self.S3_RAW_TEST_FILE)
+    
+    @computed_field
+    @property
+    def S3_STORE_PREFIX(self):
+        return os.path.join( self.S3_BUCKET , self.S3_ETL_PY_PREFIX)
+
+    @computed_field
+    @property
+    def S3_PAYMENT_TYPE_FILE(self):
+        return os.path.join(self.S3_STORE_PREFIX, self.PAYMENT_TYPE_FILE)
+
+    @computed_field
+    @property
+    def S3_RATE_CODE_FILE(self):
+        return os.path.join(self.S3_STORE_PREFIX, self.RATE_CODE_FILE)
+
+    @computed_field
+    @property
+    def S3_TAXI_ZONE_FILE(self):
+        return os.path.join(self.S3_STORE_PREFIX, self.TAXI_ZONE_FILE)
+
+    @computed_field
+    @property
+    def S3_VENDOR_LOOKUP_FILE(self):
+        return os.path.join(self.S3_STORE_PREFIX, self.VENDOR_LOOKUP_FILE)
+
+
 
     model_config = SettingsConfigDict(env_file="conf/.env", env_file_encoding="utf-8", extra='allow')
     
--- pyspark/etl_spark_emr.py
+++ pyspark/etl_spark_emr.py
@@ -1,19 +1,66 @@
+from codecs import lookup
 from pyspark.sql import SparkSession, functions as f
-from config import etl_settings
+from config import etl_settings as s
 import os
+import functools
+from pyspark.sql.types import (StructType, StructField, StringType, LongType, IntegerType,
+                               DoubleType, TimestampType)
+
+
+
+
 
 
 def read_file_names():
 
-    if etl_settings.EXEC_ENV == "emr":
-        raw = os.path.join(etl_settings.S3_BUCKET, etl_settings.S3_RAW_KEY, etl_settings.FILE_NAME)
-        processed = os.path.join(etl_settings.S3_BUCKET, etl_settings.S3_PRCSD_KEY, etl_settings.FILE_NAME)
+    if s.EXEC_ENV == "emr":
+        raw = os.path.join(s.S3_BUCKET, s.S3_RAW_KEY, s.FILE_NAME)
+        processed = os.path.join(s.S3_BUCKET, s.S3_PRCSD_KEY, s.FILE_NAME)
     else:
-        raw = os.path.join(etl_settings.LOCAL_RAW_DATA_PATH, etl_settings.FILE_NAME)
-        processed = os.path.join(etl_settings.LOCAL_PRCSD_DATA_PATH, etl_settings.FILE_NAME)
+        raw = os.path.join(s.LOCAL_RAW_DATA_PATH, s.FILE_NAME)
+        processed = os.path.join(s.LOCAL_PRCSD_DATA_PATH, s.FILE_NAME)
     return (raw, processed)
 
+def read_extras_names():
+    if s.EXEC_ENV == "emr":
+        processed = os.path.join(s.S3_BUCKET, s.S3_EXTRAS)
+    else:
+        processed = os.path.join(s.LOCAL_PRCSD_DATA_PATH, s.LOCAL_EXTRAS ,s.FILE_NAME)
+    return (processed)
 
+
+def parse_lookups(file_name):
+    out = {}
+    with open(file_name, "r") as f:
+        line = f.readline()
+        for line in f:
+            arr = line.split(",")
+            out[int(arr[0])] = arr[1]            
+    return out
+
+                
+trips_schema = StructType([
+    StructField("VendorID", IntegerType(), True),
+    StructField("tpep_pickup_datetime", TimestampType(), True),
+    StructField("tpep_dropoff_datetime", TimestampType(), True),
+    StructField("passenger_count", LongType(), True),
+    StructField("trip_distance", DoubleType(), True),
+    StructField("RatecodeID", LongType(), True),
+    StructField("store_and_fwd_flag", StringType(), True),
+    StructField("PULocationID", IntegerType(), True),
+    StructField("DOLocationID", IntegerType(), True),
+    StructField("payment_code", LongType(), True),
+    StructField("fare_amount", DoubleType(), True),
+    StructField("extra", DoubleType(), True),
+    StructField("mta_tax", DoubleType(), True),
+    StructField("tip_amount", DoubleType(), True),
+    StructField("tolls_amount", DoubleType(), True),
+    StructField("improvement_surcharge", DoubleType(), True),
+    StructField("total_amount", DoubleType(), True),
+    StructField("congestion_surcharge", DoubleType(), True),
+    StructField("Airport_fee", DoubleType(), True),
+    StructField("cbd_congestion_fee", DoubleType(), True)
+])
 
 spark = SparkSession.builder.appName("NYC_taxi_ETL").getOrCreate()
 spark.sparkContext.setLogLevel("error")
@@ -23,15 +70,54 @@ des_data_path = "/home/ec2-user/NYC_taxi/data/processed/"
 file_name = "yellow_tripdata_2025-01.parquet"
 
 raw_file, processed_file = read_file_names()
+dq_stats = read_extras_names()
 
-df = spark.read.parquet(raw_file)
+import_config = {
+    "header":"true",
+    "mode":"FAILFAST"
+    }
+
+df = spark.read.schema(trips_schema).options(import_config).parquet(raw_file)
 df.printSchema()
 df.show(10)
 df = df.filter((f.col("fare_amount") != 0) & (f.col("PULocationID") != 0) & (f.col("DOLocationID") != 0))
 df = df.withColumn("IsWeekend", f.when(f.dayofweek(f.col("tpep_pickup_datetime")).isin(1,7), 1).otherwise(0))
 
+paymenttype_dict = parse_lookups(s.S3_PAYMENT_TYPE_FILE)
+ratecode_dict = parse_lookups(s.S3_RATE_CODE_FILE)
+taxizone_dict = parse_lookups(s.S3_TAXI_ZONE_FILE)
+vendor_dict = parse_lookups(s.S3_VENDOR_LOOKUP_FILE)
+
+payment_type = spark.sparkContext.broadcast(paymenttype_dict)
+rate_code = spark.sparkContext.broadcast(ratecode_dict)
+taxi_zone = spark.sparkContext.broadcast(taxizone_dict)
+vendor_name = spark.sparkContext.broadcast(vendor_dict)
+
+def lookup_payment(lookup_dict, key):
+    return lookup_dict.get(key)
+
+udf_payment_type = f.udf(functools.partial(lookup_payment, payment_type.value))
+udf_rate_code = f.udf(functools.partial(lookup_payment, rate_code.value))
+udf_taxi_zone = f.udf(functools.partial(lookup_payment, taxi_zone.value))
+udf_vendor_name = f.udf(functools.partial(lookup_payment, vendor_name.value))
+
+df = df.withColumn("payment_type", udf_payment_type(f.col("event")))\
+    .withColumn("rate_type", udf_rate_code(f.col("RatecodeID")))\
+    .withColumn("PULocation", udf_taxi_zone(f.col("PULocationID")))\
+    .withColumn("DOLocation", udf_taxi_zone(f.col("DOLocationID")))\
+    .withColumn("Vendor", udf_taxi_zone(f.col("VendorID")))
+
+stats_df = df.agg(
+    f.sum("total_amount").alias("total_amount"),
+    f.count("VendorID").alias("row_count")
+    )
+
+
+
 print(df.count())
 
+stats_df = df.count()
+df.write.mode("overwrite").csv(dq_stats)
 df.write.mode("overwrite").parquet(processed_file)
 
 spark.stop()
